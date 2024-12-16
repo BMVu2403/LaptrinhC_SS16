8c90b2831f11719e6e057cad4873520976529c68
BMVu2403 <buiminhvu06hn@gmail.com>
2024-12-16 08:59:01 +0700

    Add files via upload

---
+++ ss16-ex3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void sum(int a,int b,int*result){
+    *result=a+b;  
+}
+int main(){
+    int x=6,y=13; 
+    int result=0;    
+    sum(x,y,&result);
+    printf("Tong cua %d va %d la:%d\n",x,y,result);
+    return 0;
+}
+
